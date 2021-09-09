bbb0bb96742647720e89935e0c6c0be0a8107c20
Niraj Kamdar <nirajkamdar27@pm.me>
2021-09-09 18:21:17 +0530

    fix: tests

--- verifier-client/src/__tests__/integration/integration.spec.ts
+++ verifier-client/src/__tests__/integration/integration.spec.ts
@@ -7,12 +7,12 @@ import { RegistryAuthority } from "./RegistryAuthority";
 import { PackageOwner } from "./PackageOwner";
 import { EnsApi } from "./ens/EnsApi";
 import { VotingMachine__factory } from "../../typechain";
-import * as VotingMachine from "../../deployments/localhost/VotingMachine.json"
+import * as VotingMachine from "../../deployments/localhost/VotingMachine.json";
 import { VerifierStateInfo } from "../../VerifierStateInfo";
 import { buildDependencyContainer } from "../../di/buildDependencyContainer";
 import { VerifierClient } from "../../services/VerifierClient";
 
-require('custom-env').env('local');
+require("custom-env").env("local");
 
 jest.setTimeout(200000);
 
@@ -27,12 +27,26 @@ describe("Start local chain", () => {
   });
 
   beforeEach(async () => {
-    await runCommand('docker-compose up -d', !shouldLog, `${__dirname}/../../../`);
-    await runCommand('yarn hardhat deploy --network localhost', !shouldLog, `${__dirname}/../../../../`);
+    const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+    await runCommand(
+      "docker-compose up -d",
+      !shouldLog,
+      `${__dirname}/../../../`
+    );
+    await sleep(10000);
+    await runCommand(
+      "yarn hardhat deploy --network localhost",
+      !shouldLog,
+      `${__dirname}/../../../../`
+    );
   });
 
   afterEach(async () => {
-    await runCommand('docker-compose down', !shouldLog, `${__dirname}/../../../`);
+    await runCommand(
+      "docker-compose down",
+      !shouldLog,
+      `${__dirname}/../../../`
+    );
   });
 
   it("start", async () => {
@@ -40,16 +54,27 @@ describe("Start local chain", () => {
     const l1ChainName = "l1-chain-name";
     const l2ChainName = "l2-chain-name";
 
-    var provider = ethers.providers.getDefaultProvider(process.env.PROVIDER_NETWORK);
+    var provider = ethers.providers.getDefaultProvider(
+      process.env.PROVIDER_NETWORK
+    );
     const ipfsClient = create({
-      url: process.env.IPFS_URI
+      url: process.env.IPFS_URI,
     });
 
-    var packageOwner = new PackageOwner(provider, process.env.PACKAGE_OWNER_PRIVATE_KEY!);
-    var authority = new RegistryAuthority(provider, process.env.REGISTRY_AUTHORITY_PRIVATE_KEY!);
+    var packageOwner = new PackageOwner(
+      provider,
+      process.env.PACKAGE_OWNER_PRIVATE_KEY!
+    );
+    var authority = new RegistryAuthority(
+      provider,
+      process.env.REGISTRY_AUTHORITY_PRIVATE_KEY!
+    );
     var ens = new EnsApi();
 
-    const verifierSigner = new ethers.Wallet(process.env.VERIFIER_PRIVATE_KEY!, provider);
+    const verifierSigner = new ethers.Wallet(
+      process.env.VERIFIER_PRIVATE_KEY!,
+      provider
+    );
     await authority.authorizeVerifiers([await verifierSigner.getAddress()]);
 
     await ens.init(provider);
@@ -79,27 +104,30 @@ describe("Start local chain", () => {
 });
 
 async function runCommand(command: string, quiet: boolean, cwd: string) {
-
   if (!quiet) {
-    console.log(`> ${command}`)
+    console.log(`> ${command}`);
   }
 
   return new Promise((resolve, reject) => {
-    const callback = (err: ExecException | null, stdout: string, stderr: string) => {
+    const callback = (
+      err: ExecException | null,
+      stdout: string,
+      stderr: string
+    ) => {
       if (err) {
-        console.error(err)
-        reject(err)
+        console.error(err);
+        reject(err);
       } else {
         if (!quiet) {
           // the *entire* stdout and stderr (buffered)
-          console.log(`stdout: ${stdout}`)
-          console.log(`stderr: ${stderr}`)
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
         }
 
         resolve(null);
       }
-    }
+    };
 
-    exec(command, { cwd: cwd }, callback)
-  })
+    exec(command, { cwd: cwd }, callback);
+  });
 }
