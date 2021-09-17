923d60e9601e12785a57927d31364c38177d5e63
Jure Bogunovic <bogunovij@gmail.com>
2021-09-17 13:43:56 +0200

    prettier and eslint added ignore dirs

--- packages/verifier-client/jest.config.js
+++ packages/verifier-client/jest.config.js
@@ -4,9 +4,7 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   modulePathIgnorePatterns: [],
-  roots: [
-    "./src/__tests__/jest"
-  ],
+  roots: ["./src/__tests__/jest"],
   globals: {
     "ts-jest": {
       diagnostics: false,
