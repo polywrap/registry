9d199ef8e4bdaa52452b1548111cb249b9907985
Niraj Kamdar <nirajkamdar27@pm.me>
2021-09-23 14:31:11 +0530

    feat(VersionResolver): add getVersionInfo function

--- packages/registry/contracts/versioning/registry/VersionResolver.sol
+++ packages/registry/contracts/versioning/registry/VersionResolver.sol
@@ -27,4 +27,37 @@ abstract contract VersionResolver is Registry {
     bytes32 concreteVersionId = resolveToLeaf(versionNodeId);
     return versionNodes[concreteVersionId].location;
   }
+
+  function getLatestVersionInfo(bytes32 packageId)
+    public
+    view
+    returns (
+      bytes32 packageId,
+      uint256 majorVersion,
+      uint256 minorVersion,
+      uint256 patchVersion,
+      string memory location
+    )
+  {
+    PackageVersion storage packageNode = versionNodes[packageId];
+    uint256 majorVersion = packageNode.latestSubVersion;
+    bytes32 majorNodeId = keccak256(abi.encodePacked(packageId, majorVersion));
+
+    PackageVersion storage majorNode = versionNodes[majorNodeId];
+    uint256 minorVersion = majorNode.latestSubVersion;
+    bytes32 minorNodeId = keccak256(
+      abi.encodePacked(majorNodeId, minorVersion)
+    );
+
+    PackageVersion storage minorNode = versionNodes[minorNodeId];
+    uint256 patchVersion = minorNode.latestSubVersion;
+    bytes32 patchNodeId = keccak256(
+      abi.encodePacked(minorNodeId, patchVersion)
+    );
+
+    PackageVersion storage patchNode = versionNodes[patchNodeId];
+    string memory location = patchNode.location;
+
+    return (packageId, majorVersion, minorVersion, patchVersion, location);
+  }
 }
