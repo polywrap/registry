0799b2c52bd1d6d16255fa61c540ef259e8d17e6
Jure Bogunovic <bogunovij@gmail.com>
2021-10-11 20:53:44 +0200

    removed extra /ipfs prefix

--- packages/verifier/client/api/src/__tests__/hardhat/integration/helpers/IpfsPublisher.ts
+++ packages/verifier/client/api/src/__tests__/hardhat/integration/helpers/IpfsPublisher.ts
@@ -1,4 +1,9 @@
-import { Uri, Web3ApiClient } from "@web3api/client-js";
+import {
+  deserializeWeb3ApiManifest,
+  Uri,
+  Web3ApiClient,
+  Web3ApiManifest,
+} from "@web3api/client-js";
 import fs from "fs";
 
 export class IpfsPublisher {
@@ -14,6 +19,15 @@ export class IpfsPublisher {
     const uri = new Uri(`w3://ipfs/${cid}`);
 
     this.polywrapClient["_apiCache"].set(uri.uri, {
+      getManifest: (): Promise<Web3ApiManifest> => {
+        return Promise.resolve(
+          deserializeWeb3ApiManifest(
+            fs.readFileSync(buildPath + "/web3api.yaml", {
+              encoding: "utf-8",
+            })
+          )
+        );
+      },
       getSchema: (): Promise<string> => {
         return Promise.resolve(
           fs.readFileSync(buildPath + "/schema.graphql", {
