df97bb9fde55312cdc7aa236c0671a039e42d4b6
Niraj Kamdar <nirajkamdar27@pm.me>
2021-10-14 16:04:44 +0530

    fix: error handler

--- packages/registry-js/src/errorHandler.ts
+++ packages/registry-js/src/errorHandler.ts
@@ -10,6 +10,49 @@ import {
 import { FunctionResult } from "./types/FunctionResult";
 import { MaybeAsync } from "./types/MaybeAsync";
 
+function parseContractError<TReturn>(
+  errorObj: Record<string, any>
+): ContractCallResult<TReturn> {
+  if (errorObj["code"] in errors) {
+    if ("tx" in errorObj) {
+      const baseError = errorObj as BaseTransactionError;
+      const responseBody = JSON.parse(
+        baseError.error.error.body
+      ) as ErrorResponseBody;
+      if (responseBody.error.message.startsWith("execution reverted: ")) {
+        const revertMessage = responseBody.error.message.substring(20).trim();
+        const error: ContractError = {
+          error: baseError,
+          revertMessage: revertMessage,
+        };
+        return [error, undefined];
+      } else {
+        const error: ContractError = {
+          error: baseError,
+          revertMessage: "Reverted without any message",
+        };
+        return [error, undefined];
+      }
+    } else {
+      if (errorObj["reason"]) {
+        const error: ContractError = {
+          error: errorObj as BaseContractError,
+          revertMessage: errorObj["reason"],
+        };
+        return [error, undefined];
+      } else {
+        const error: ContractError = {
+          error: errorObj as BaseContractError,
+          revertMessage: "Reverted without any message",
+        };
+        return [error, undefined];
+      }
+    }
+  }
+
+  throw errorObj;
+}
+
 export function handleContractError<TArgs extends Array<unknown>, TReturn>(
   func: (...args: TArgs) => MaybeAsync<TReturn>
 ) {
@@ -18,61 +61,34 @@ export function handleContractError<TArgs extends Array<unknown>, TReturn>(
       const res = func(...args);
 
       if (isPromise(res)) {
-        const result = (res
+        const [error, data] = (res
           .then((res: any) => [undefined, res])
           .catch((error) => [
             error,
             undefined,
           ]) as unknown) as ContractCallResult<TReturn>;
-        return result;
+
+        if (error) {
+          return parseContractError(error as Record<string, unknown>);
+        } else {
+          return [undefined, data];
+        }
       } else {
         return [undefined, res];
       }
     } catch (err) {
-      const errorObj = err as Record<string, any>;
-
-      if (errorObj["code"] in errors) {
-        if ("tx" in errorObj) {
-          const baseError = errorObj as BaseTransactionError;
-          const responseBody = JSON.parse(
-            baseError.error.error.body
-          ) as ErrorResponseBody;
-          if (responseBody.error.message.startsWith("execution reverted: ")) {
-            const revertMessage = responseBody.error.message
-              .substring(20)
-              .trim();
-            const error: ContractError = {
-              error: baseError,
-              revertMessage: revertMessage,
-            };
-            return [error, undefined];
-          } else {
-            const error: ContractError = {
-              error: baseError,
-              revertMessage: "Reverted without any message",
-            };
-            return [error, undefined];
-          }
-        } else {
-          if (errorObj["reason"]) {
-            const error: ContractError = {
-              error: errorObj as BaseContractError,
-              revertMessage: errorObj["reason"],
-            };
-            return [error, undefined];
-          } else {
-            const error: ContractError = {
-              error: errorObj as BaseContractError,
-              revertMessage: "Reverted without any message",
-            };
-            return [error, undefined];
-          }
-        }
-      }
-
-      throw err;
+      return parseContractError(err as Record<string, unknown>);
     }
   };
+}
+
+function parseError(error: Error, funcName: string): Error {
+  if (error.message) {
+    return error;
+  } else {
+    error.message = `Error: call to the function: ${funcName} failed`;
+    return error;
+  }
 }
 
 export function handleError<TArgs extends Array<unknown>, TReturn>(
@@ -83,18 +99,22 @@ export function handleError<TArgs extends Array<unknown>, TReturn>(
       const res = func(...args);
 
       if (isPromise(res)) {
-        const result = (res
+        const [error, data] = (res
           .then((res: any) => [undefined, res])
           .catch((error) => [
-            error,
+            error as Error,
             undefined,
           ]) as unknown) as FunctionResult<TReturn>;
-        return result;
+        if (error) {
+          return [parseError(error, func.name), undefined];
+        } else {
+          return [undefined, data];
+        }
       } else {
         return [undefined, res];
       }
     } catch (err) {
-      return [err as Error, undefined];
+      return [parseError(err as Error, func.name), undefined];
     }
   };
 }
