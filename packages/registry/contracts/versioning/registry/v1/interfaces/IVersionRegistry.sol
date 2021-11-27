f635baee8ac6255248a453591dce07a4dfb08b0b
Jure Bogunovic <bogunovij@gmail.com>
2021-11-27 22:32:55 +0100

    removed virtual from interface methods

--- packages/registry/contracts/versioning/registry/v1/interfaces/IVersionRegistry.sol
+++ packages/registry/contracts/versioning/registry/v1/interfaces/IVersionRegistry.sol
@@ -5,10 +5,10 @@ interface IVersionRegistry {
 	event TrustedVersionPublisherSet(address trustedVersionPublisher);
 	event VersionPublished(bytes32 indexed packageId, bytes32 indexed versionNodeId, bytes version, bytes32 buildMetadata, string location);
 	
-	function publishVersion(bytes32 packageId, bytes memory version, bytes32 buildMetadata, string memory location) external virtual returns (bytes32 nodeId);
-	function versionIds(bytes32 packageId, uint256 start, uint256 count) external virtual view returns (bytes32[] memory);
-	function versionCount(bytes32 packageId) external virtual view returns (uint256);
-	function versionLocation(bytes32 nodeId) external virtual view returns (string memory);
-	function versionBuildMetadata(bytes32 nodeId) external virtual view returns (bytes32);
-	function version(bytes32 nodeId) external virtual view returns (bool leaf, bool created, uint8 level, uint256 latestPrereleaseVersion, uint256 latestReleaseVersion, bytes32 buildMetadata, string memory location);
+	function publishVersion(bytes32 packageId, bytes memory version, bytes32 buildMetadata, string memory location) external returns (bytes32 nodeId);
+	function versionIds(bytes32 packageId, uint256 start, uint256 count) external view returns (bytes32[] memory);
+	function versionCount(bytes32 packageId) external view returns (uint256);
+	function versionLocation(bytes32 nodeId) external view returns (string memory);
+	function versionBuildMetadata(bytes32 nodeId) external view returns (bytes32);
+	function version(bytes32 nodeId) external view returns (bool leaf, bool created, uint8 level, uint256 latestPrereleaseVersion, uint256 latestReleaseVersion, bytes32 buildMetadata, string memory location);
 }
--- packages/registry/contracts/versioning/registry/v1/interfaces/IVersionResolver.sol
+++ packages/registry/contracts/versioning/registry/v1/interfaces/IVersionResolver.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.10;
 
 interface IVersionResolver {
-	function latestReleaseNode(bytes32 versionNodeId) external virtual view returns (bytes32 nodeId);
-	function latestPrereleaseNode(bytes32 versionNodeId) external virtual view returns (bytes32 nodeId);
-	function latestReleaseLocation(bytes32 versionNodeId) external virtual view returns (string memory location);
-	function latestPrereleaseLocation(bytes32 versionNodeId) external virtual view returns (string memory location);
+	function latestReleaseNode(bytes32 versionNodeId) external view returns (bytes32 nodeId);
+	function latestPrereleaseNode(bytes32 versionNodeId) external view returns (bytes32 nodeId);
+	function latestReleaseLocation(bytes32 versionNodeId) external view returns (string memory location);
+	function latestPrereleaseLocation(bytes32 versionNodeId) external view returns (string memory location);
 }
